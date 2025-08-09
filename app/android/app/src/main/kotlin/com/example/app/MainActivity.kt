59086ea17a436349e40171a4f6da0b5f5257e0a4
Muflih-uk <muhammedmuflih875@gmail.com>
2025-08-09 19:39:56 +0530

    Start Again

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.app // <-- change to your actual package
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // Default FlutterActivity is fine; context will be provided by Flutter engine
-}
